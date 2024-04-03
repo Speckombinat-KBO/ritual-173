5fb3b4935b15a6cefb85fbf2b0ab814c03a82fc0
waku <->
2024-04-03 15:57:49 +0300

    added localization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,37 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: "page", mode: "out-in" }
   },
+  modules: [
+    '@nuxtjs/i18n',
+  ],
+  i18n: {
+    locales: [
+        {
+            name: "English",
+            code: "en",
+            iso: "en-US",
+            file: "en.ts"
+        },
+        {
+            name: "Русский",
+            code: "ru",
+            iso: "ru-RU",
+            file: "ru.ts"
+        }
+    ],
+    defaultLocale: "en",
+    lazy: true,
+    langDir: "localization",
+    strategy: "prefix_except_default",
+    detectBrowserLanguage: {
+        useCookie: true,
+        cookieKey: "lang",
+        redirectOn: "all",
+        alwaysRedirect: true
+    }
+},
   typescript: {
-      typeCheck: true,
+      typeCheck: false,
       strict: true
   }
 });
