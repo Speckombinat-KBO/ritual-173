6c08b6a10fc7c985d92a1453f0f0bee83f5387f4
waku <->
2024-04-04 13:23:52 +0300

    components

---
+++ components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <header>
+        <div class="header-full">
+            <div class="header-top">
+                
+            </div>
+            <div class="header-bottom">
+            </div>
+        </div>
+    </header>
+</template>
+
+
+<style scoped>
+header {
+    height: 56px;
+    background-color: rgb(230, 230, 230);
+}
+.header-full {
+    display: flex;
+    flex-direction: column;
+}
+</style>
